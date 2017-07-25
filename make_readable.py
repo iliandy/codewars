bba73fe8f9280ddda1e201d4b9f11dd296e292c2
iliandy <iandyli@gmail.com>
2017-07-24 23:10:04 -0500

    completed make_readable.py algo

---
+++ make_readable.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/52685f7382004e774f0001f7/train/python
+
+def make_readable(sec):
+    hh = (sec/60**2) % 100
+    mm = (sec/60) % 60
+    ss = sec % 60
+    return "{:02}:{:02}:{:02}".format(hh, mm, ss)
+
+
+print make_readable(359999)
