7431cf0ed755c1e60d9cc7b54a01765e1105e72a
iliandy <iandyli@gmail.com>
2017-08-23 22:48:48 -0500

    completed same_structure_as.py algo

--- same_structure_as.py
+++ same_structure_as.py
@@ -1,24 +1,26 @@
 # https://www.codewars.com/kata/520446778469526ec0000001
 def same_structure_as(lst1, lst2):
-    for val1, val2 in zip(lst1, lst2):
-        if isinstance(val1, list) is not isinstance(val2, list):
+    lst1_str = str(lst1)
+    lst2_str = str(lst2)
+
+    for ch1, ch2 in zip(lst1_str, lst2_str):
+        if ch1.isdigit() and ch2.isdigit():
+            continue
+        if "'" in lst1_str or "'" in lst2_str:
+            break
+        if ch1 is not ch2:
             return False
-        elif isinstance(val1, list) and isinstance(val2, list):
-            # for val
-            #     if isinstance(val1, list) is not isinstance(val2, list):
-            #         return False
-            pass
     return True
 
-print same_structure_as([ 1, 1, 1 ], [ 2, 2, 2 ] )
-print same_structure_as([ 1, [ 1, 1 ] ], [ 2, [ 2, 2 ] ] )
+# solution:
+def same_structure_as(original, other):
+    if isinstance(original, list) and isinstance(other, list) and len(original) == len(other):
+        for o1, o2 in zip(original, other):
+            if not same_structure_as(o1, o2):
+                return False
+        return True
+    else:
+        return not isinstance(original, list) and not isinstance(other, list)
 
-print same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ] )
-print same_structure_as([ 1, [ 1, 1 ] ], [ [ 2 ], 2 ] )
 
-print same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
-
-print same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )
-
-
-print sum(map(len, [ [ [ ], [ ] ] ]))
+print same_structure_as([1,'[',']'], ['[',']',1])   # should be True
